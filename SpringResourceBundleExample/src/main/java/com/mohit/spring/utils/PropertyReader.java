8041317a41ca94ba8235fbe0bea4a1f84eae7392
Mohit Agarwal <agrawalmohit@live.com>
2016-12-18 14:09:56 +0530

    corrected configuration for message source bundle

--- SpringResourceBundleExample/src/main/java/com/mohit/spring/utils/PropertyReader.java
+++ SpringResourceBundleExample/src/main/java/com/mohit/spring/utils/PropertyReader.java
@@ -2,17 +2,21 @@ package com.mohit.spring.utils;
 
 import java.util.Locale;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
 
-public class PropertyReader {
+@Component
+public class PropertyReader implements ApplicationContextAware {
 
+	private static ApplicationContext context;
 	private static ResourceBundleMessageSource messageResourceBundle;
 	private static Locale locale = new Locale("en");
 
 	public static void loadResources() {
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
 		messageResourceBundle = (ResourceBundleMessageSource) context.getBean("messageSource");
 	}
 
@@ -24,6 +28,11 @@ public class PropertyReader {
 			throw new Exception("illegal argument");
 		}
 		return (String) messageResourceBundle.getMessage(key, args, locale);
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext arg0) throws BeansException {
+		context = arg0;
 	}
 
 }
